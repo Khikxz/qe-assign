4914e136a0fcfa9158ab239135f8fed67877edf0
Khikxz <140308359+Khikxz@users.noreply.github.com>
2024-02-05 20:43:00 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+My assignment output uses html for the markup, css for styling, and javascript for some interaction.
+I also used the SimpleLightbox library via cdn to view images in a gallery as slides.
+
+My main challenges for this assignment are the given time, and to create a website from scratch or without template/design to follow. I based my output from OneQuantum Philippines website, but on Sunday the said website was not available so I checked the OneQuantum website for additional references. I believe I could do more than what I created when in right circumstances
+
+To view my output, just visit khikxz.github.io/qe-assign
---
+++ js/qe-assign.js
@@ -0,0 +1,47 @@
+const gallery = document.querySelector('.hero-gallery');
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+
+// INDEX GALLERY
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  let simpleGallery = new SimpleLightbox('.hero-gallery a');
+  simpleGallery.on('show.simplelightbox', function () {
+  });
+});
+
+// MOBILE BURGER MENU
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const toggleMenu = () => {
+    const isMenuOpen =
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+      
+  };
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+  });
+})();
+
+// SERVICES ACCORDION
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
